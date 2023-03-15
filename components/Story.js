d49dfcd92dc7007e992bf28a6864d1e620602f75
yamicodes <dhruv2krawat@gmail.com>
2023-03-15 20:34:09 +0530

    First Commit

---
+++ components/Story.js
@@ -0,0 +1,21 @@
+export default function Story(story){
+    return `
+    <div class='story'>
+        <div>
+            <span class="gray"> ${story.index}</span>
+            <span class="upvote"> ▲ </span>
+            <a href='${story.url}'> ${story.title}</a>
+            <span> (${story.domain})</span>
+        </div>
+        <div class="gray">
+            <span> ${story.points} by ${story.user} ${story.time_ago} </span>
+            |
+            <a href='#/item?id=${story.id}'> ${story.comments_count}</a>
+            |
+            <span class="favourites">
+                <img class="heart" src="https://icon.now.sh/heart/ccc">
+                Add To Favorites
+            </span>
+        </div>
+    </div>`;
+}
---
+++ pages/stories.js
@@ -0,0 +1,25 @@
+import view from '../utils/view.js'
+import Story from '../components/Story.js'
+
+export default async function Stories(path) {
+    const stories = await getStories(path);
+    const hasStories = stories.length > 0
+
+    view.innerHTML = `<div>
+    ${hasStories ? stories.map((story, index) => Story({...story,index: index + 1})).join('') : 'No Stories'}
+    </div>`
+}
+
+
+const getStories = async (path) => {
+    const isHomeRoute = path === '/';
+    const isNewRoute = path ==='/new';
+    if (isHomeRoute) {
+        path = '/news';  
+      } else if (isNewRoute) {
+        path = '/newest';  
+      } 
+    const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
+    const stories = await response.json();
+    return stories;
+}
